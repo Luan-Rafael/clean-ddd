8695eee0e0def8e02b7bcfd197c1fed08fb591e1
Luan-Rafael <luan.tec18@gmail.com>
2025-02-26 13:10:57 -0300

    feat: create value object slug and test

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("it should be able to create a new slug from text", () => {
+
+    const slugText = Slug.createFromText("Example question title")
+
+    expect(slugText.value).toEqual("example-question-title")
+})
