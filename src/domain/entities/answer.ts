2d688e838deef71fa993d43c496901b3c11cf162
Luan-Rafael <luan.tec18@gmail.com>
2025-02-26 14:05:17 -0300

    feat: add getters and setters for Answer and Question entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -9,10 +9,44 @@ interface AnswerProps {
     createdAt: Date,
     updatedAt?: Date
 }
+
 export class Answer extends Entity<AnswerProps> {
-    get content() {
-        return this.props.content;
+    get questionId() {
+        return this.props.questionId
     }
+
+    get authorId() {
+        return this.props.authorId
+    }
+
+    get content() {
+        return this.props.content
+    }
+
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+        return this.content
+            .substring(0, 120)
+            .trimEnd()
+            .concat('...')
+    }
+
+    set content(content: string) {
+        this.props.content = this.content
+        this.touch()
+    }
+
+    private touch() {
+        this.props.updatedAt = new Date();
+    }
+
     static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
         const answer = new Answer({
             ...props,
