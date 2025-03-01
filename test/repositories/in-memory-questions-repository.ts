02024131fd8498a0118b33e7c580ba22ee71b2cd
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 16:58:30 -0300

    feat(repository): implement findById and delete in-memory for QuestionRepository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -3,15 +3,29 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    return question ?? null
+  }
+
   async findBySlug(slug: Slug): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug.value)
 
     return question ?? null
   }
 
-  public items: Question[] = []
-
-  async create(question: Question): Promise<void> {
+  async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const index = this.items.findIndex((item) => item.id === question.id)
+
+    if (index >= 0) {
+      this.items.splice(index, 1)
+    }
   }
 }
