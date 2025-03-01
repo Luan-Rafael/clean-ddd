38b9fbc88a073afb709f3d2b4c4dd1ec71284424
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 11:31:36 -0300

    feat: create question

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,9 +1,13 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => { }
+}
+
 test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase({
-    create: async () => {},
-  })
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
   const answer = await answerQuestion.execute({
     instructorId: '1',
---
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -0,0 +1,31 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Question } from "../../enterprise/entities/question";
+import { QuestionsRepository } from "../repositories/questions-repository";
+
+interface CreateQuestionUseCaseRequest {
+    authorId: string,
+    title: string,
+    content: string
+}
+
+interface CreateQuestionUseResponse {
+    question: Question
+}
+
+export class CreateQuestionUseCase {
+    constructor(private questionsRepository: QuestionsRepository) { }
+
+    async execute(question: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseResponse> {
+
+        const newQuestion = Question.create({
+            authorId: new UniqueEntityId(question.authorId),
+            title: question.title,
+            content: question.content
+        })
+
+        await this.questionsRepository.create(newQuestion)
+
+        return { question: newQuestion }
+
+    }
+}
