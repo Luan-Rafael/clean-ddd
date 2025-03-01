0d24a78bea9713522036e2e8afb93dabe25bd492
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 15:22:38 -0300

    feat(in-memory-questions-repository): implement findBySlug method

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,7 +1,14 @@
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  async findBySlug(slug: Slug): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug.value)
+
+    return question ?? null
+  }
+
   public items: Question[] = []
 
   async create(question: Question): Promise<void> {
