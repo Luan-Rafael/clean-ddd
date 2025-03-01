6078c0c1e779f662787b7d6cff4db7d84e3d27c0
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 18:33:14 -0300

    feat(usecase): add edit question usecase

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,37 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  questionId: string
+  authorId: string
+  title: string
+  content: string
+}
+interface EditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+
+  async execute({
+    questionId,
+    authorId,
+    content,
+    title,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not Allowed.')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionRepository.save(question)
+
+    return {}
+  }
+}
