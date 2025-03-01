75bfd934eed2692348eeb98f75741ed612703253
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 18:32:50 -0300

    test(usecase): add test for edit question usecase functionality

---
+++ src/domain/forum/application/use-cases/edit-question .spec.ts
@@ -0,0 +1,58 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from '../../../../../test/factories/make-questions'
+import { InMemoryQuestionsRepository } from '../../../../../test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: 'question-1',
+      authorId: 'author-1',
+      title: 'New Title',
+      content: 'New Content',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'New Title',
+      content: 'New Content',
+    })
+    expect(inMemoryQuestionsRepository.items[0].title).toBe('New Title')
+  })
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: 'question-1',
+        authorId: 'author-2',
+        title: 'New Title',
+        content: 'New Content',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
