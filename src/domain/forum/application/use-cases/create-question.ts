d46def8ba794178097654726f885c18918d316d0
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 12:03:12 -0300

    feat(use-case): implement question creation

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,31 +1,31 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
-import { Question } from "../../enterprise/entities/question";
-import { QuestionsRepository } from "../repositories/questions-repository";
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface CreateQuestionUseCaseRequest {
-    authorId: string,
-    title: string,
-    content: string
+  authorId: string
+  title: string
+  content: string
 }
 
 interface CreateQuestionUseResponse {
-    question: Question
+  question: Question
 }
 
 export class CreateQuestionUseCase {
-    constructor(private questionsRepository: QuestionsRepository) { }
+  constructor(private questionsRepository: QuestionsRepository) {}
 
-    async execute(question: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseResponse> {
+  async execute(
+    question: CreateQuestionUseCaseRequest,
+  ): Promise<CreateQuestionUseResponse> {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityId(question.authorId),
+      title: question.title,
+      content: question.content,
+    })
 
-        const newQuestion = Question.create({
-            authorId: new UniqueEntityId(question.authorId),
-            title: question.title,
-            content: question.content
-        })
+    await this.questionsRepository.create(newQuestion)
 
-        await this.questionsRepository.create(newQuestion)
-
-        return { question: newQuestion }
-
-    }
+    return { question: newQuestion }
+  }
 }
