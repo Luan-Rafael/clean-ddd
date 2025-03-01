050a4b42d86d67dc74a9f32fa1b13097460e8230
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 16:25:35 -0300

    refactor(factory): integrate faker for generating random data

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -1,18 +1,23 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps>) {
-  const question = Question.create({
-    authorId: new UniqueEntityId('1'),
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    content: 'Exemple Content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps>,
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId('1'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
