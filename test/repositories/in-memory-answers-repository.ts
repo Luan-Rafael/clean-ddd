ff7c0921cedd487185b0f813393efd714c438cff
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 17:51:40 -0300

    feat(repository): implement findById and delete in-memory for AnswerRepository

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -3,6 +3,17 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
+  async findById(answerId: string) {
+    const answer = this.items.find((e) => e.id.toString() === answerId)
+    return answer ?? null
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const findIndex = this.items.findIndex((item) => item.id === answer.id)
+    this.items.splice(findIndex, 1)
+  }
+
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
   }
