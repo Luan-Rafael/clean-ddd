33e84fdd424e2e9f9c705752f17cbe883385b807
Luan-Rafael <luan.tec18@gmail.com>
2025-03-02 10:56:54 -0300

    feat(repository): implement findManyByQuestionId method in memory repository

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -7,6 +8,15 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async findById(answerId: string) {
     const answer = this.items.find((e) => e.id.toString() === answerId)
     return answer ?? null
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer): Promise<void> {
