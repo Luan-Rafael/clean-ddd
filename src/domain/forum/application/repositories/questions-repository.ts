3a974e79cc5a01c24150e8a87244ef27a65a083b
Luan-Rafael <luan.tec18@gmail.com>
2025-03-02 00:09:40 -0300

    feat(repository): add findManyRecent method for retrieving recent questions

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,11 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>
   findBySlug(slug: Slug): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[] | []>
   create(question: Question): Promise<void>
   save(question: Question): Promise<void>
   delete(question: Question): Promise<void>
