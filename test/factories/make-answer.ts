b7a44e910c9aa9406b5369a389b49eca84b68b78
Luan-Rafael <luan.tec18@gmail.com>
2025-03-01 23:18:07 -0300

    chore(factory): set parameter as nullable

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -3,7 +3,7 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
 
 export function makeAnswer(
-  override: Partial<AnswerProps>,
+  override?: Partial<AnswerProps>,
   id?: UniqueEntityId,
 ) {
   const question = Answer.create(
